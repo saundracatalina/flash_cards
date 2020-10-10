13c50a3793750dbe70c536c0033e3db33ebe49c4
Saundra <saundracatalina@gmail.com>
2020-10-10 15:44:56 -0600

    Add cards_in_category method

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,7 +1,18 @@
+require 'pry'
+
 class Deck
   attr_reader :cards
   def initialize(cards)
     @cards = cards
   end
 
+  def count
+    cards.count
+  end
+
+  def cards_in_category(category)
+    @cards.select do |card|
+    card.category == :STEM
+  end
+  end
 end
