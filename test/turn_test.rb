fb719c38d9dfe4fcc736047a2cf46c2ac7893102
Saundra <saundracatalina@gmail.com>
2020-10-10 15:57:59 -0600

    Add setup method with instance variables

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -6,29 +6,28 @@ require 'pry'
 
 class TurnTest < Minitest::Test
 
+  def setup
+    @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @turn_1 = Turn.new("Juneau", @card_1)
+    @card_2 = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    @turn_2 = Turn.new("Saturn", @card_2)
+  end
+
   def test_it_exists
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card_1)
-    assert_instance_of Turn, turn
+    assert_instance_of Turn, @turn_1
   end
 
   def test_card_info
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card_1)
-    assert_equal card_1, turn.card
-    assert_equal "Juneau", turn.guess
-    assert_equal true, turn.correct?
+    assert_equal @card_1, @turn_1.card
+    assert_equal "Juneau", @turn_1.guess
+    assert_equal true, @turn_1.correct?
   end
 
   def test_if_user_gets_feedback_when_guess_is_correct
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card_1)
-    assert_equal "Correct!", turn.feedback
+    assert_equal "Correct!", @turn_1.feedback
   end
 
   def test_if_user_gets_feedback_when_guess_is_incorrect
-    card_2 = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
-    turn = Turn.new("Saturn", card_2)
-    assert_equal "Incorrect.", turn.feedback
+    assert_equal "Incorrect.", @turn_2.feedback
   end
 end
