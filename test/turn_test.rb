d5e596d6ed9aef52641d352a40fdd717be3346f6
Saundra <saundracatalina@gmail.com>
2020-10-09 20:29:37 -0600

    Add instance_of and card_info tests

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -1,11 +1,27 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require '.lib/turn'
+require './lib/turn'
+require './lib/card'
+require 'pry'
 
 class TurnTest < Minitest::Test
 
   def test_it_exists
-    turn = Turn.new()
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card_1)
     assert_instance_of Turn, turn
+  end
+
+  def test_card_info
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card_1)
+    assert_equal "Juneau", turn.guess
+    assert_equal true, turn.correct?
+  end
+
+  def test_if_guess_is_correct
+  end
+
+  def test_if_user_gets_feedback
   end
 end
