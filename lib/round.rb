4a4bfcc1d8af744e9b9de60f1810598ad376cc70
Saundra <saundracatalina@gmail.com>
2020-10-11 15:27:56 -0600

    Add take_turn method and new_turn test

--- lib/round.rb
+++ lib/round.rb
@@ -1,17 +1,22 @@
 require 'pry'
 class Round
-  attr_reader :deck, :turns
+  attr_reader :deck, :turns, :card_tracker, :current_turn
   def initialize(deck)
     @deck = deck
     @turns = []
+    @card_tracker = 0
+    @current_turn
   end
 
   def current_card
-    @deck.cards[0]
+    @deck.cards[card_tracker]
   end
 
-  # def take_turn(guess)
-  #   @guess
-  # end
+  def take_turn(guess)
+    @current_turn = Turn.new(guess, @deck.cards[card_tracker])
+    # @turns << turn
+  end
+
+
 
 end
--- test/round_test.rb
+++ test/round_test.rb
@@ -12,25 +12,33 @@ class RoundTest < Minitest::Test
     @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-    @deck = Deck.new([@card_1, @card_2, @card_3])
-    @round = Round.new(@deck)
+    @deck_1 = Deck.new([@card_1, @card_2, @card_3])
+    @round_1 = Round.new(@deck_1)
   end
 
   def test_deck_can_be_accessed
-    assert_equal @deck, @round.deck
+    assert_equal @deck_1, @round_1.deck
   end
 
   def test_turns_starts_as_empty_array
-    assert_equal [], @round.turns
+    assert_equal [], @round_1.turns
   end
 
   def test_current_card_is_card_1
-    assert_equal @card_1, @round.current_card
+    assert_equal @card_1, @round_1.current_card
   end
 
-  # def test_take_new_turn_info
-  #   new_turn = @round.take_turn("Juneau")
-  #   assert_equal @turn_1.guess, new_turn
+  def test_take_new_turn_info
+    new_turn = @round_1.take_turn("Juneau")
+    assert_equal new_turn, @round_1.current_turn
+    # binding.pry
+  end
+
+  # def test_class_of_new_turn_variable
+  #   assert_equal Turn, new_turn.class
   # end
+
+  def test_if_new_turn_correct
+  end
 
 end
