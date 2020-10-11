278af9abe8d1b28aa29ba0ec8aebcc5f8a11357c
Saundra <saundracatalina@gmail.com>
2020-10-10 20:48:17 -0600

    Add current_card method

--- lib/round.rb
+++ lib/round.rb
@@ -1,7 +1,17 @@
+require 'pry'
 class Round
   attr_reader :deck, :turns
   def initialize(deck)
     @deck = deck
     @turns = []
   end
+
+  def current_card
+    @deck.cards[0]
+  end
+
+  # def take_turn(guess)
+  #   @guess
+  # end
+
 end
